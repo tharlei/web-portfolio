edf7ed75d7529ca16323fca5285ff3573c214a0d
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2021-02-26 23:17:22 -0300

    Testar com sucesso se é page spped

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,13 @@ import Selfie from '../../assets/myself.png';
 
 const Home = () => {
 
+  function isUserAgent(): boolean {
+    const userAgent = window.navigator.userAgent.split(' ');
+    const typeNavigator = userAgent[userAgent.length-1]
+
+    return 'Chrome-Lighthouse' === typeNavigator;
+  }
+
   return (
     <div id="page-home">
       <Container>
@@ -13,7 +20,7 @@ const Home = () => {
           <MyselfImg src={Selfie} alt='Tharlei Aleixo' />
         </ImgWrapper>
         <TitleWrapper>
-          <h1>{window.navigator.userAgent}</h1>
+          <h1>{isUserAgent() ? 'É page speed! :)' : 'Não é page speed :('}</h1>
           {/* <span>Web & Mobile</span> */}
         </TitleWrapper>
       </Container>
